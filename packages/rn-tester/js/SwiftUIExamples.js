8af6e7e702bea109aff637435313d1eb7ad158f1
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2020-08-27 09:23:25 +0200

    Clean up RNTester's JS code and add simple View example

---
+++ packages/rn-tester/js/SwiftUIExamples.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class SwiftUIExamples extends React.PureComponent {
+  render() {
+    return <View style={{ width: 200, height: 300, backgroundColor: 'blue', alignSelf: 'center', marginTop: 100  }} />;
+  }
+}
+
+export default SwiftUIExamples;
